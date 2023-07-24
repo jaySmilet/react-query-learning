c3a7e80bb4212c4784a06ed9881cce5193bb51c7
jaySmilet <36183760+jaySmilet@users.noreply.github.com>
2023-07-24 23:49:29 +0530

    1 file modified

--- src/components/RQSuperHeroes.page.tsx
+++ src/components/RQSuperHeroes.page.tsx
@@ -1,7 +1,38 @@
 import * as React from 'react';
+import { useQuery } from 'react-query';
+import { SuperHero } from '../models/models';
 
 const RQSuperHeroesPage = () => {
-  return <div>RQSuperHeroesPage</div>;
+  const { isLoading, data } = useQuery('super-heroes', () =>
+    fetch('http://localhost:3000/supeheroes').then((res) => res.json())
+  );
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Super Hero</th>
+            <th>Fan Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((value: SuperHero) => {
+            return (
+              <tr>
+                <td>{value.id}</td>
+                <td>{value.name}</td>
+                <td>{value.alterEgo}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
 };
 
 export default RQSuperHeroesPage;
