1db5c693a30cb97daf97dd52ead8075ceeb0058d
jaySmilet <36183760+jaySmilet@users.noreply.github.com>
2023-07-24 23:11:45 +0530

    1 file modified

--- src/components/SuperHeroes.page.tsx
+++ src/components/SuperHeroes.page.tsx
@@ -5,22 +5,50 @@ const SuperHeroesPage = () => {
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
   const [data, setData] = React.useState<SuperHero[]>([]);
   React.useEffect(() => {
-    fetch(
-      'https://stackblitzstarters59zzjg-ssec--3000--bec01ace.local-corp.webcontainer.io/supeheroes',
-      {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }
-    )
-      .then((res) => {
-        console.log(res);
+    setIsLoading(true);
+    fetch('http://localhost:3000/supeheroes', {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res: Response) => {
+        if (res.status === 200) {
+          res.json().then((v: SuperHero[]) => setData(v));
+          setIsLoading(false);
+        }
       })
       .catch((err) => {
         console.log(err);
+        setIsLoading(false);
       });
   }, []);
-  return <div>SuperHeroesPage</div>;
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Super Hero</th>
+            <th>Fan Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((value: SuperHero) => {
+            return (
+              <tr>
+                <td>{value.id}</td>
+                <td>{value.name}</td>
+                <td>{value.alterEgo}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
 };
 
 export default SuperHeroesPage;
