5df9c9c4b00b5511668939f5729c8880966f7064
jaySmilet <36183760+jaySmilet@users.noreply.github.com>
2023-07-27 01:40:51 +0530

    1 file modified

--- src/components/RQSuperHeroes.page.tsx
+++ src/components/RQSuperHeroes.page.tsx
@@ -35,7 +35,7 @@ const RQSuperHeroesPage = () => {
         <tbody>
           {data.map((value: SuperHero) => {
             return (
-              <tr>
+              <tr key={value.id}>
                 <td>{value.id}</td>
                 <td>{value.name}</td>
                 <td>{value.alterEgo}</td>
